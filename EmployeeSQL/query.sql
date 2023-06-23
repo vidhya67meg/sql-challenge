a31f075ed41a4771170620ce7e597f783d9880b7
vidhya67meg <mvidhya.ft@gmail.com>
2023-06-22 21:23:40 -0700

    tables and query

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,57 @@
+--List the employee number, last name, first name, sex, and salary of each employee
+select employees.emp_no, employees.last_name, employees.first_name,
+employees.sex, salaries.salary
+from employees
+inner join salaries on 
+employees.emp_no = salaries.emp_no;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986 
+select first_name, last_name, hire_date
+from employees
+WHERE EXTRACT(YEAR FROM hire_date::date) = 1986;
+
+--List the manager of each department along with their department number, department name, 
+--employee number, last name, and first name 
+SELECT dept.dept_no, dept.dept_name, employees.emp_no, 
+employees.last_name,employees.first_name
+FROM dept
+JOIN dept_manager ON dept.dept_no = dept_manager.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, 
+--last name, first name, and department name 
+select employees.emp_no, employees.last_name, employees.first_name, dept.dept_name
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join dept on dept_emp.dept_no = dept.dept_no;
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose 
+--last name begins with the letter B 
+select first_name, last_name, sex
+from employees
+where lower(first_name) = 'hercules' and lower(last_name) like 'b%';
+
+--List each employee in the Sales department, including their employee number, last name, 
+--and first name
+select employees.emp_no, employees.last_name, employees.first_name, dept.dept_name
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join dept on dept_emp.dept_no = dept.dept_no
+where dept.dept_name = 'Sales';
+
+--List each employee in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name
+
+select employees.emp_no, employees.last_name, employees.first_name, dept.dept_name
+from employees
+join dept_emp on employees.emp_no = dept_emp.emp_no
+join dept on dept_emp.dept_no = dept.dept_no
+where dept.dept_name = 'Sales' or dept.dept_name = 'Development'
+order by dept.dept_name;
+
+--List the frequency counts, in descending order, of all the employee last names (that is, how many 
+--employees share each last name) 
+select last_name, count(*) as count
+from employees
+GROUP BY last_name
+order by count desc;
---
+++ EmployeeSQL/tables.sql
@@ -0,0 +1,72 @@
+﻿
+CREATE TABLE dept (
+    dept_no VARCHAR(10)   NOT NULL,
+    dept_name VARCHAR(30)   NOT NULL,
+    CONSTRAINT pk_dept PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE dept_emp (
+    emp_no VARCHAR(10)   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no,dept_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(10)   NOT NULL,
+    emp_no VARCHAR(10)   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE employees (
+    emp_no VARCHAR(10)   NOT NULL,
+    emp_title_id VARCHAR(30)   NOT NULL,
+    birth_date VARCHAR(12)   NOT NULL,
+    first_name VARCHAR(50)   NOT NULL,
+    last_name VARCHAR(50)   NOT NULL,
+    sex VARCHAR(10)   NOT NULL,
+    hire_date VARCHAR(12)   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE salaries (
+    emp_no VARCHAR(10)   NOT NULL,
+    salary INTEGER   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(20)   NOT NULL,
+    title VARCHAR(20)   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES dept (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES dept (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
